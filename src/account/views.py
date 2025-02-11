3853cabfd321477666c856eb40138053c6b64a31
Lytoonn <lytoonytgg@gmail.com>
2025-02-11 12:44:09 +0000

    forms creation

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+async def home(request: HttpResponse) -> HttpResponse:
+    """ ... """
+    return render(request, 'account/home.html', {'x': 33})
+
+async def register(request: HttpResponse) -> HttpResponse:
+    """ ... """
+    return render(request, 'account/register.html')
+
+async def login(request: HttpResponse) -> HttpResponse:
+    """ ... """
+    return render(request, 'account/login.html')
