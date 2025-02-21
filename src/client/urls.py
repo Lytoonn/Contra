18aa5201728e4b8ff9ea83474a6427be051686a5
Lytoonn <lytoonytgg@gmail.com>
2025-02-21 17:23:27 +0000

    Client functionalities and start paypal

--- src/client/urls.py
+++ src/client/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import dashboard
+from . import views
 
 urlpatterns = [
-    path('dashboard/', dashboard, name = 'client-dashboard')
+    path('dashboard/', views.dashboard, name = 'client-dashboard'),
+    path('browse-articles/', views.browse_articles, name = 'client-browse-articles'),
+    path('subscribe-plan/', views.subscribe_plan, name = 'client-subscribe-plan'),
 ]
--- src/client/views.py
+++ src/client/views.py
@@ -1,14 +1,55 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import aget_user
 
+from client.models import Subscription
+from writer.models import Article
 from common.auth import aclient_required
 from common.django_utils import arender
+
+PlanChoices = Subscription.PlanChoices
 
 @aclient_required
 async def dashboard(request: HttpRequest) -> HttpResponse:
     """
     This is the client dashboard.
     """
-    return await arender(request, 'client/dashboard.html')
+    user = await aget_user(request)
+    try:
+        subscription = await Subscription.objects.aget(user = user, is_active = True)
+        plan = PlanChoices(subscription.plan)
+        subscription_plan = plan.label
+    except ObjectDoesNotExist:
+        subscription_plan = 'No subscription yet.'
+    context = {'subscription_plan': subscription_plan}
+    return await arender(request, 'client/dashboard.html', context)
+
+@aclient_required
+async def browse_articles(request: HttpRequest) -> HttpResponse:
+    """
+    This is the client's page to browse articles.
+    """
+    user = await aget_user(request)
+    try:
+        subscription = await Subscription.objects.aget(user = user, is_active = True)
+        has_subscription = True
+        plan = PlanChoices(subscription.plan)
+        if plan == PlanChoices.STANDARD:
+            articles = Article.objects.filter(is_premium = False)
+        else:
+            articles = Article.objects.all()
+    except ObjectDoesNotExist:
+        has_subscription = False
+        articles = []
+    
+    context = {'has_subscription': has_subscription, 'articles': articles}
+    return await arender(request, 'client/browse-articles.html', context)
+
+@aclient_required
+async def subscribe_plan(request: HttpRequest) -> HttpResponse:
+    """
+    This is the client's page to subscription plans.
+    """
+    return await arender(request, 'client/subscribe-plan.html')
